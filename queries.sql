4932516cd12470926a98200d9bae06a944b6133e
ravana99 <tomas.lopes@tecnico.ulisboa.pt>
2019-11-23 19:22:05 +0000

    added queries.sql (slight modifications to populate and schema)

---
+++ queries.sql
@@ -0,0 +1,42 @@
+/*Query #1*/
+
+SELECT nome
+FROM item NATURAL JOIN local_publico NATURAL JOIN incidencia 
+WHERE id=item_id 
+GROUP BY nome, latitude, longitude 
+HAVING COUNT(*)>=ALL (
+    SELECT COUNT(*) 
+    FROM item NATURAL JOIN incidencia 
+    WHERE id=item_id 
+    GROUP BY latitude, longitude
+);
+
+/*Query #2*/
+
+SELECT email 
+FROM anomalia NATURAL JOIN anomalia_traducao NATURAL JOIN incidencia NATURAL JOIN utilizador_regular 
+WHERE ts>='2019-01-01 00:00:00' AND ts<='2019-06-30 23:59:59' AND id=anomalia_id 
+GROUP BY email
+HAVING COUNT(*)>=ALL (
+    SELECT COUNT(*) 
+    FROM anomalia NATURAL JOIN anomalia_traducao NATURAL JOIN incidencia NATURAL JOIN utilizador_regular 
+    WHERE ts>='2019-01-01 00:00:00' AND ts<='2019-06-30 23:59:59' AND id=anomalia_id 
+    GROUP BY email
+);
+
+/*Query #3*/
+
+SELECT email 
+FROM utilizador 
+WHERE email NOT IN (
+    SELECT DISTINCT email 
+    FROM (
+        (SELECT email, latitude, longitude FROM utilizador,local_publico WHERE latitude>39.336775) 
+        EXCEPT 
+        (SELECT DISTINCT email, latitude, longitude 
+        FROM anomalia,incidencia,item NATURAL JOIN local_publico 
+        WHERE item_id=item.id AND anomalia_id=anomalia.id AND extract(year FROM ts)=2019 AND latitude>39.336775)
+    ) AS R
+);
+
+/*Query #4*/
